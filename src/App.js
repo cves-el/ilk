d8687744043fd808b39ac835c70904044094309e
cves <elias.volgger@gmail.com>
2024-02-05 15:12:33 +0100

    modal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ModalEingabeAuftrag from "./component/modal_eingabe_auftrag";
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+        <div style={{ height: '10%', backgroundColor: 'lightblue', padding: '10px' }}>
+          Oberer Bereich
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'row', height: '75%' }}>
+          <div style={{ backgroundColor: 'lightgreen', padding: '10px', width: '20vh' }}>
+            <ModalEingabeAuftrag />
+          </div>
+          <div style={{ backgroundColor: 'lightcoral', padding: '10px', width: '100%' }}>
+            Aufgaben
+          </div>
+          <div style={{ width: '20vh' }}>
+            Chat
+          </div>
+        </div>
+        <div style={{ position: 'absolute', bottom: '0', height: '15%', width: '100%', backgroundColor: 'lightgray', padding: '10px' }}>
+          Unterer Bereich
+        </div>
+      </div>
     </div>
   );
 }
